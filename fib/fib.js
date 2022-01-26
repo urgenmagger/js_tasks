2292603ff092f88777a625d757e0f01b468cfd4d
urgenmagger <evgen.magger@gmail.com>
2022-01-26 19:56:04 +0300

    Add iter fibonacci

--- fib/fib.js
+++ fib/fib.js
@@ -5,4 +5,16 @@ const fib = (n) => {
     return fib(n - 1) + fib(n - 2);
   }
 };
-console.log(fib(40)) 
+//console.log(fib(40));
+
+const fib_iter = (n) => {
+  let previos = 0;
+  let next = 1;
+  while(n--) {
+    current = previos + next;
+    previos = next;
+    next = current;
+  }
+  return current;
+};
+console.log(fib_iter(8));
